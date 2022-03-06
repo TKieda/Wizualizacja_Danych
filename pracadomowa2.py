edd29fc153c09de82067d8376b8b9e4a5b62905d
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-03-06 16:54:10 +0100

    "zadania"

--- pracadomowa2.py
+++ pracadomowa2.py
@@ -1,2 +1,20 @@
 #### Zad1. ####
-print("Zadanie 1")
+print("\n Zadanie 1 \n")
+
+ulubione_sporty=['siatkówka','piłka ręczna', 'hokej']
+print(ulubione_sporty)
+ulubione_sporty.reverse()
+ulubione_sporty.append('koszykówka')
+ulubione_sporty.append('piłka nożna')
+print(ulubione_sporty)
+
+#### Zad2. ####
+print("\n Zadanie 2 \n")
+
+slownik={"gg":"goodgame", "wp":"wellplay", "hf":"havefun"}
+print(slownik["gg"])
+print(slownik["wp"])
+print(slownik["hf"])
+
+#### Zad3. ####
+print("\n Zadanie 3 \n")
