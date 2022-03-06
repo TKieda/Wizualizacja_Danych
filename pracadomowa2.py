33708d137f2bcd100761d76ed3593a7566d976e1
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-03-06 19:42:13 +0100

    "zadania"

--- pracadomowa2.py
+++ pracadomowa2.py
@@ -1,3 +1,6 @@
+import sys as system
+import math
+
 #### Zad1. ####
 print("\n Zadanie 1 \n")
 
@@ -11,10 +14,32 @@ print(ulubione_sporty)
 #### Zad2. ####
 print("\n Zadanie 2 \n")
 
-slownik={"gg":"goodgame", "wp":"wellplay", "hf":"havefun"}
-print(slownik["gg"])
-print(slownik["wp"])
-print(slownik["hf"])
+slownik1={"gg":"goodgame", "wp":"wellplay", "hf":"havefun"}
+print(slownik1["gg"])
+print(slownik1["wp"])
+print(slownik1["hf"])
 
 #### Zad3. ####
 print("\n Zadanie 3 \n")
+
+slownik2={"W3":"Witcher 3","OW":"Overwatch","PVR":"Pavlov VR","CIV6":"Civilization 6"}
+print("Liczba elementów w słowniku:", len(slownik2))
+
+#### Zad4. ####
+print("\n Zadanie 4 \n")
+
+l=input("Napisz zdanie:")
+print("Ilość liter 'a' wynosi:", l.count("a")+l.count("A"))
+
+#### Zad5. ####
+print("\n Zadanie 5 \n")
+
+system.stdout.write("Podaj trzy liczby: ")
+liczby = system.stdin.readline().split(' ')
+if (len(liczby)==3):
+    dzialanie=math.pow(int(liczby[0]),int(liczby[1]))+int(liczby[2])
+    print("Wynik: ", end="")
+    system.stdout.write(str(dzialanie))
+else:
+    print("Wprowadzono za mało bądź za dużo liczb")
+
