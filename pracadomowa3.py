3edcc3ffd71429c10e4d6d4c315190b235cbc3d2
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-03-22 19:45:42 +0100

    "zadania"

--- pracadomowa3.py
+++ pracadomowa3.py
@@ -11,33 +11,33 @@ print("\nZadanie 1\n")
 # B = {1,4,16,…,4^7}
 # C = {x: x∈B i x jest liczbą podzielną przez 2}
 
-# a = [1-x for x in range(1,11)]
-# b = [4**i for i in range(8)]
-# c = [x for x in b if x % 2 == 0]
-# print(a)
-# print(b)
-# print(c)
+a = [1-x for x in range(1,11)]
+b = [4**i for i in range(8)]
+c = [x for x in b if x % 2 == 0]
+print(a)
+print(b)
+print(c)
 
 
 
 ##### Zad2 #####
 print("\nZadanie 2\n")
 
-# lista1 = []
-# for x in range(0,10):
-#      r = random.randint(1, 100)
-#      lista1.append(r)
-# lista2 = [i for i in lista1 if i % 2 == 0]
-# print(lista1)
-# print(lista2)
-#
-##### Zad3 #####
-# print("\nZadanie 3\n")
-#
-# slownik1 = {"czekolada":"szt", "pomidory":"kg", "woda":"l", "ziemniaki":"kg", "bułki":"szt", "jajka":"szt"}
-# print(slownik1)
-# slownik2 = {value: key for key, value in slownik1.items() if value == "szt" }
-# print(slownik2)
+lista1 = []
+for x in range(0,10):
+     r = random.randint(1, 100)
+     lista1.append(r)
+lista2 = [i for i in lista1 if i % 2 == 0]
+print(lista1)
+print(lista2)
+
+#### Zad3 #####
+print("\nZadanie 3\n")
+
+slownik1 = {"czekolada":"szt", "pomidory":"kg", "woda":"l", "ziemniaki":"kg", "bułki":"szt", "jajka":"szt"}
+print(slownik1)
+slownik2 = {value: key for key, value in slownik1.items() if value == "szt" }
+print(slownik2)
 
 ##### Zad4 #####
 print("\nZadanie 4\n")
