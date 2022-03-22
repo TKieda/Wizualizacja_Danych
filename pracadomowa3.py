3531f697fb8a55ea1b0cba564c8cb9201cc9f492
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-03-22 21:07:15 +0100

    "zadania"

--- pracadomowa3.py
+++ pracadomowa3.py
@@ -63,14 +63,14 @@ print(trapez())
 ##### Zad6 #####
 print("\nZadanie 6\n")
 
-# def ciag(a1 = 1, b = 4, ile = 10):
-#      ciag = []
-#      for wynik in range(0, ile, 1):
-#           wynik = a1 * b
-#           a1 += 1
-#           ciag.append(wynik)
-#      print(ciag)
-# print(ciag())
+def ciag(a1 = 1, b = 4, ile = 10):
+     ciag = []
+     for wynik in range(0, ile, 1):
+          wynik = a1 * b
+          a1 += 1
+          ciag.append(wynik)
+     print(ciag)
+print(ciag())
 
 ##### Zad7 #####
 print("\nZadanie 7\n")
