748b9b33431b37334539af93c9ae356438066c62
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-03-26 13:26:54 +0100

    "zadania"

---
+++ kolokwium1.py
@@ -0,0 +1,44 @@
+import decimal
+import math
+######Zestaw A######
+
+######Zad 1######
+
+print("Zadanie 1")
+wynik=math.pow(math.sqrt(5/36),1/5)+math.e**5+math.cos(15)**2
+print(round(wynik,2))
+print("\n")
+######Zad 2######
+print("Zadanie 2")
+lista1=[1,2,3,4,5,5.5,6.5,7.5]
+lista2=[]
+lista3=[]
+for i in lista1:
+    if i % 1==0:
+        lista2.append(i)
+    else:
+        lista3.append(i)
+
+print(lista2)
+print(lista3)
+print("\n")
+
+######Zad 3######
+print("Zadanie 3")
+
+def lista(lista4=[1,2,3,4,"a","b","c","d"]):
+    lista5=[]
+    for i in lista4:
+        try:
+            int(i)
+            lista5.append(i)
+        except:
+            str(i)
+    return lista5
+print(sum(lista()))
+print("\n")
+
+######Zad 4######
+print("Zadanie 4")
+l=int(input("Podaj liczbę w systemie dziesiętnym:"))
+print(oct(l))
