ee2920250b451952419e74911a59c921a4c3e83d
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-05-02 20:43:22 +0200

    "zadania"

--- pracadomowa4.py
+++ pracadomowa4.py
@@ -3,11 +3,13 @@ import numpy as np
 
 #### Zad 1 ####
 print("\nZadanie 1\n")
+
 a = np.arange(3,48,3)
 print(a)
 
 #### Zad 2 ####
 print("\nZadanie 2\n")
+
 a = np.array([1.2, 4.332, 7.4])
 print("Typ tablicy 'a' to:", a.dtype)
 print(a,"\n")
