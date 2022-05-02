063166c366227fe4d8946b72f97637e5da42c9e6
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-05-02 18:24:31 +0200

    "zadania"

--- ćwiczenia.py
+++ ćwiczenia.py
@@ -265,92 +265,103 @@ print(sys.version)
 
 ###########Numpy, tablice itd.###################
 
-a=np.array([1,2,3], dtype='float64')
-print(a)
-a=np.arange(1,5,0.5)
-print(a)
-print(a.dtype) # typ tablicy
-print(type(a))
-print(a.shape) # rozmiar tablicy
-print(a.ndim) # ilowymiarowa
+# a=np.array([1,2,3], dtype='float64')
+# print("Typ float", a)
+# b=np.array([1,2,3], dtype='int32')
+# print("Typ int", b)
+# a=np.arange(1,5,0.5)
+# print("Tablica zaczynająca się od 1, mająca 5 elementów i zmieniająca się o 0.5",a)
+# print("Wypisanie typu tablicy:", a.dtype) # typ tablicy
+# #print(type(a))
+# print("Rozmiar tablicy:", a.shape) # rozmiar tablicy
+# print("Iluwymiarowa tablica:", a.ndim) # ilowymiarowa
+#
+# print("\nTablica 2x2:")
+# b = np.array([np.arange(2), np.arange(2)])
+# print(b)
+# print("Rozmiar:", b.shape)
+# print("Wymiar:", b.ndim)
+#
+#
+# print("\nTablica z samymi zerami:")
+# z = np.zeros((5,5), dtype='int32') # tablica z samymi zerami
+# print(z)
+# print("\nTablica z samymi jedynkami:")
+# j = np.ones((5,5)) # tablica z samymi jedynkami
+# print(j)
+# print("\nTablica z przypadkowymi wartościami:")
+# p = np.empty((2,2), dtype='int32')
+# print(p)
+#
+# print("\nTablica 2x2 gdzie we wskazanym miejscu na tablicy wstawiono cyfrę 2:")
+# p[1][0] = 2
+# print(p)
+#
+# print("\nPięcioelementowa tablica z pominięciem wartości 2")
+# a = np.linspace(1, 2, 5, endpoint=False) # pięcioelementowa tablica z pominięciem wartości 2
+# print(a)
 
-b = np.array([np.arange(2), np.arange(2)])
-print(b)
-print(b.shape)
-print(b.ndim)
+# print("\nTablica 5x3 (5 wierszy, 3 kolumny)")
+# b, c = np.indices((5,3)) # wartość po kolumnach
+# print(b)
+# print(c)
+# print("Wartość wybrana po indeksie:",b[3][2])
+# print("Wartość wybrana po indeksie:",c[3][2])
 
-z = np.zeros((5,5), dtype='int32') # tablica z samymi zerami
-print(z)
-print(z.dtype)
-j = np.ones((5,5)) # tablica z samymi jedynkami
-print(j)
-p = np.empty((2,2), dtype='int32')
-print(p)
-
-p[1][1] = 2
-print(p)
-
-a = np.linspace(1, 2, 5, endpoint=False) # pięcioelementowa tablica z pominięciem wartości 2
-print(a)
-
-b, c = np.indices((5, 5)) # wartość po kolumnach
-#print(b[0][1][1])
-print(b)
-print(c)
+print("\nTablica 4x3 (4 wiersze, 3 kolumny)")
 d,e = np.mgrid[0:5, 0:5] # watrość po wierszach
-
 print(d)
 print(e)
-
-f= np.diag([x for x in range(5)],k=-1) # wektor po przekątnej 5x5, wartość k zmienia wektor tworząc przy tym nowy wiersz
-print(f)
-
-g=np.fromiter(range(5), dtype='int')
-print(g)
-
-marcin = 'Marcin'
-marcin = b'Marcin'
-mar = np.frombuffer(marcin, dtype='S6')
-print(mar)
-
-mar_1 = np.array(list(marcin))
-print(mar_1)
-mar_2 = np.array(list(b'Marcin')) # kod ASCI
-print(mar_2)
-mar_3 = np.fromiter(marcin, dtype='S1')
-mar_4 = np.fromiter(marcin, dtype='U1')
-print(mar_3)
-print(mar_4)
-
-a = np.ones((2,2))
-b = np.ones((2,2))
-c = a + b
-print(a)
-print(b)
-print(c)
-
-a = np.arange(10)
-print(a)
-s = slice(2,7,2)
-print(a[s])
-s = range(2,7,2)
-print(a[s])
-
-print(a[2:7:2])
-print(a[1:5])
-
-a = np.arange(25)
-mat = a.reshape((5,5))
-print(a)
-print(mat)
-print(mat[1:, 1:])
-print(mat[:, 1:2])
-print(mat[2:5, 1:3])
-
-a = np.array([[1,2,3], [4,5,6], [7,8,9],[10,11,12]])
-print(a)
-
-rows = np.array([[0, 0], [3, 3]])
-cols = np.array([[0, 2], [0, 2]])
-b=a[rows, cols]
-print(b)
+#
+# f= np.diag([x for x in range(5)],k=-1) # wektor po przekątnej 5x5, wartość k zmienia wektor tworząc przy tym nowy wiersz
+# print(f)
+#
+# g=np.fromiter(range(5), dtype='int')
+# print(g)
+#
+# marcin = 'Marcin'
+# marcin = b'Marcin'
+# mar = np.frombuffer(marcin, dtype='S6')
+# print(mar)
+#
+# mar_1 = np.array(list(marcin))
+# print(mar_1)
+# mar_2 = np.array(list(b'Marcin')) # kod ASCI
+# print(mar_2)
+# mar_3 = np.fromiter(marcin, dtype='S1')
+# mar_4 = np.fromiter(marcin, dtype='U1')
+# print(mar_3)
+# print(mar_4)
+#
+# a = np.ones((2,2))
+# b = np.ones((2,2))
+# c = a + b
+# print(a)
+# print(b)
+# print(c)
+#
+# a = np.arange(10)
+# print(a)
+# s = slice(2,7,2)
+# print(a[s])
+# s = range(2,7,2)
+# print(a[s])
+#
+# print(a[2:7:2])
+# print(a[1:5])
+#
+# a = np.arange(25)
+# mat = a.reshape((5,5))
+# print(a)
+# print(mat)
+# print(mat[1:, 1:])
+# print(mat[:, 1:2])
+# print(mat[2:5, 1:3])
+#
+# a = np.array([[1,2,3], [4,5,6], [7,8,9],[10,11,12]])
+# print(a)
+#
+# rows = np.array([[0, 0], [3, 3]])
+# cols = np.array([[0, 2], [0, 2]])
+# b=a[rows, cols]
+# print(b)
