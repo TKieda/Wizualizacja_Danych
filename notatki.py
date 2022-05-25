f219c38f8d05f4dc5a83311eee99230b08773620
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-05-25 19:40:24 +0200

    "zadania"

--- notatki.py
+++ notatki.py
@@ -2,6 +2,8 @@ import sys
 import numpy as np
 import math
 import random
+from PIL import Image
+import matplotlib.pyplot as plt
 
 # print(sys.version)
 #
@@ -397,3 +399,157 @@ import random
 # print("Liczb jest:", len(lista11))
 # print("Liczb jest:", len(lista12))
 
+#######matplotlib 3.5.1
+
+# grupa = df.groupby('Kontynent').agg({'Populacja':[sum]})
+# grupa.plot(kind='bar', xlabel='Kontynent', ylabel='Populacja w mld', rot=0, title='Populacja na kontynentach', legend=True)
+
+# wykres = grupa.plot.bar()
+# wykres.set_xlabel('Kontynenty')
+# wykres.set_ylabel('Populacja w mld')
+# wykres.tick_params(axis='x', labelrotation=0)
+# wykres.set_title('Populacja na kontynentach')
+# plt.show()
+
+# grupa = df3.groupby('Imię i nazwisko').agg({'Wartość zamówienia':['sum']})
+# print(grupa)
+# grupa.plot(kind='pie', subplots=True, autopct='%.2f %%', fontsize=20, figsize=(8,6), colors=['red','green'], legend=(3,0))
+# plt.legend(loc='upper left')
+# plt.savefig('plot.png')
+# plt.show()
+
+# seria = pd.Series(np.random.randn(1000))
+# seria = seria.cumsum()
+# print(seria)
+#
+# seria.plot()
+# plt.show()
+
+# plt.plot([1,2,3,4],[1,4,9,16],'ro:',label='linia')
+# plt.ylabel('wartości z wektora')
+# plt.show()
+#
+# plt.plot([1,2,3,4],[1,4,9,16],'r:')
+# plt.plot([1,2,3,4],[1,4,9,16],'bo')
+# plt.axis([0,6,0,20]) #ustawienie max i min na osiach
+# plt.show()
+
+
+# t = np.arange(0,5,0.1)
+# plt.plot(t,t,'r-',t,t**2,'b:',t,t**3,'g^')
+# plt.legend(labels=['liniowa','kwadratowa','szescienna'],loc='center left')
+# plt.show()
+# x = np.linspace(0,2,100)
+#
+# plt.plot(x,x, label='liniowa')
+# plt.plot(x,x**2,label='kwadratowa')
+# plt.plot(x,x**3,label='sześcienna')
+# plt.xlabel('etykieta na osi x')
+# plt.ylabel('etykieta na osi y')
+# plt.title('tytuł wykresu')
+# plt.savefig('plot.png')
+# plt.savefig('plot.jpg')
+# plt.show()
+# im1 = Image.Open('plot.png')
+# im1 = im1.convert('RGB')
+# im1.save('plot.jpg')
+
+## Zadanie 1 ##
+
+# x = np.arange(1,21,1)
+# plt.plot(x,(1/x))
+# plt.xlabel('Od 1 do 20')
+# plt.ylabel('1/x')
+# plt.title('Wykres')
+# plt.show()
+
+# x = np.arange(1,10,0.1)
+# y = np.sin(x)
+# plt.plot(x,y)
+# plt.xlabel('etykieta na osi x')
+# plt.ylabel('etykieta na osi y')
+# plt.title('Wykres')
+# plt.legend(labels=['Wahania nastroju kobiety przed okresem'], loc='upper center')
+# plt.show()
+
+# x1 = np.arange(0,2,0.02) # od 0 do 2 z krokiem 0.02
+# x2 = np.arange(0,2,0.02)
+#
+# y1=np.sin(2*np.pi*x1)
+# y2=np.cos(2*np.pi*x2)
+
+# plt.subplot(2,1,1)
+# plt.plot(x1, y1)
+# plt.ylabel('sin(x)')
+# plt.xlabel('x')
+# plt.title('Wykres sin(x)')
+# plt.subplot(2,1,2)
+# plt.plot(x2,y2,'r-')
+# plt.ylabel('cos(x)')
+# plt.xlabel('x')
+# plt.title('Wykres cos(x)')
+#
+# plt.subplots_adjust(hspace=0.5)
+# plt.show()
+
+# x1 = np.arange(0,2,0.02) # od 0 do 2 z krokiem 0.02
+# x2 = np.arange(0,2,0.02)
+# y1=np.sin(2*np.pi*x1)
+# y2=np.cos(2*np.pi*x2)
+# fig, axs = plt.subplots(3,2) # trzy wiersze, dwie kolumny
+# # print(type(fig))
+# # print(type(axs))
+#
+# axs[0,0].plot(x1,y1, 'g-')
+# axs[0,0].set_xlabel('x')
+# axs[0,0].set_ylabel('sin(x)')
+# axs[0,0].set_title('Wykres sin(x)')
+#
+# axs[1,1].plot(x2,y2,'r-')
+# axs[1,1].set_xlabel('x')
+# axs[1,1].set_ylabel('cos(x)')
+# axs[1,1].set_title('Wykres cos(x)')
+#
+# axs[2,0].plot(x2,y2,'r-')
+# axs[2,0].set_xlabel('x')
+# axs[2,0].set_ylabel('cos(x)')
+# axs[2,0].set_title('Wykres cos(x)')
+# fig.delaxes(axs[0,1])
+# fig.delaxes(axs[1,0])
+# fig.delaxes(axs[2,1])
+# plt.show()
+
+# data = {'a':np.arange(50),'c':np.random.randint(0,51,50),'d':np.random.randn(50)}
+# data['b'] = data['a'] + 10 * np.random.randn(50)
+# data['d'] = np.abs(data['d']) * 100
+#
+# plt.scatter(data=data,x='a', y='b',c='c', s='d') #tworzy wykres punktowy
+# plt.xlabel('Wartości z klucza a')
+# plt.ylabel('Wartości z klucza b')
+# plt.show()
+
+###Wykres kolumnowy#####
+
+# dane={'Kraj':['Belgia','Indie','Brazylia','Polska'],'Stolica': ['Bruksela', 'New Delhi','Brasilia','Warszawa'], 'Populacja':[131244234,1233432535,34234232534,12342342342], 'Kontynent':['Europa','Azja', 'Ameryka','Europa']}
+# df = pd.DataFrame(dane)
+# print(df)
+# grupa = df.groupby('Kontynent')
+# etykiety = list(grupa.groups.keys())
+# wartosci = list(grupa.agg('Populacja').sum())
+# print(etykiety)
+# print(wartosci)
+# plt.bar(x=etykiety,height=wartosci,color=['red','green','yellow'])
+# plt.xlabel('Kontynent')
+# plt.ylabel('Populacja na kontynentach')
+# plt.show()
+
+###Histogram#####
+
+x = np.random.randn(10000)
+
+plt.hist(x,bins=50, facecolor='g', alpha=0.75, density=True)
+
+plt.xlabel('Wartości')
+plt.ylabel('Prawdopodobieństwa')
+plt.title('Histogram')
+plt.show()
--- pracadomowa5.py
+++ pracadomowa5.py
@@ -1,22 +1,76 @@
 import numpy as np
 import  math
 
-#### Zad 1 ####
-print("\nZadanie 1\n")
+# #### Zad 1 ####
+# print("\nZadanie 1\n")
+#
+# a = np.array([2,3,4])
+# b = np.array([5,1,7])
+#
+# print(a*2)
+# print(b*3)
+#
+# #### Zad 2 ####
+# print("\nZadanie 2\n")
+#
+# c = np.array([2,3,4,7,5,3,2,1,3]).reshape((3,3))
+# d = np.array([6,4,2,6,7,9,0,2,3,7,3,5,8,4,2,4]).reshape((4,4))
+# print("\nMacierz 3x3\n")
+# print(c)
+#
+# print("Najmniejsze z kolumn:",c.min(axis=0))
+# print("Najmniejsze z kolumn:",c.min(axis=1))
+#
+# print("\nMacierz 4x4\n")
+# print(d)
+#
+# print("Najmniejsze z kolumn:", d.min(axis=0))
+# print("Najmniejsze z rzędu:",d.min(axis=1))
+#
+# #### Zad 3 ####
+# print("\nZadanie 3\n")
+#
+# print(a*b)
+#
+# #### Zad 4 ####
+# print("\nZadanie 4\n")
+#
+# e = np.array([2,3,4], dtype='int32')
+# f = np.array([5.2,1.1,7.2], dtype='float64')
+#
+# print(e*2)
+# print(f*3)
+#
+# #### Zad 5 ####
+# print("\nZadanie 5\n")
+#
+# g = np.array([2,3,4,7,5,8]).reshape((2,3))
+# a=[]
+# for x in g.flat:
+#     a.append(math.sin(x))
+# print(a)
+#
+# #### Zad 6 ####
+# print("\nZadanie 6\n")
+#
+# h = np.array([10,12,9,5,34,2]).reshape((2,3))
+# b=[]
+# for x in h.flat:
+#     b.append(math.cos(x))
+# print(b)
+#
+# #### Zad 7 ####
+# print("\nZadanie 7\n")
+#
+# print(g+h)
+#
+# #### Zad 8 ####
+# print("\nZadanie 8\n")
 
-a = np.array([2,3,4]).reshape((1,3))
-b = np.array([5,1,7]).reshape((1,3))
-print(a*b)
+#### Zad 9 ####
+print("\nZadanie 9\n")
 
-#### Zad 2 ####
-print("\nZadanie 2\n")
+i = np.array([[3,4,63], [24,5,16], [27,58,69]])
 
-c = np.array([2,3,4,7,5,3,2,1,3]).reshape((3,3))
-print(c)
-d = np.array([6,4,2,6,7,9,0,2,3,7,3,5,8,4,2,4]).reshape((4,4))
-print(c)
-print(c.min(axis=0))
-print(c.min(axis=1))
-print(d)
-print(d.min(axis=0))
-print(d.min(axis=1))
+for x in i.flat:
+    print("Element tablicy: ", x)
