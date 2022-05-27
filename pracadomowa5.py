459a4868e17f50aceb8167752652989f978e0fb2
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-05-27 22:14:50 +0200

    "numpy2"

--- pracadomowa5.py
+++ pracadomowa5.py
@@ -6,7 +6,8 @@ import  math
 #
 # a = np.array([2,3,4])
 # b = np.array([5,1,7])
-#
+# c = a * b
+# print(c)
 # print(a*2)
 # print(b*3)
 #
@@ -17,29 +18,31 @@ import  math
 # d = np.array([6,4,2,6,7,9,0,2,3,7,3,5,8,4,2,4]).reshape((4,4))
 # print("\nMacierz 3x3\n")
 # print(c)
-#
-# print("Najmniejsze z kolumn:",c.min(axis=0))
-# print("Najmniejsze z kolumn:",c.min(axis=1))
-#
-# print("\nMacierz 4x4\n")
+# print('')
 # print(d)
-#
-# print("Najmniejsze z kolumn:", d.min(axis=0))
-# print("Najmniejsze z rzędu:",d.min(axis=1))
+# print("Najmniejsze z kolumn:",c.min(axis=0))
+# print("Najmniejsze z wierszy:",c.min(axis=1))
+# print("Najmniejsze z kolumn:",d.min(axis=0))
+# print("Najmniejsze z wierszy:",d.min(axis=1))
+
 #
 # #### Zad 3 ####
 # print("\nZadanie 3\n")
 #
-# print(a*b)
+# a = np.array([2,3,4])
+# b = np.array([5,1,7])
+# c = np.dot(a, b)
+# print(c)
 #
 # #### Zad 4 ####
 # print("\nZadanie 4\n")
 #
-# e = np.array([2,3,4], dtype='int32')
-# f = np.array([5.2,1.1,7.2], dtype='float64')
-#
-# print(e*2)
-# print(f*3)
+# a = np.array([1, 2, 3]) # macierz z liczb całkowitych
+# b = np.linspace(0, 10, 3) # (początek, zakres, ilość liczb) macierz z liczb rzeczywistych
+# c = a * b
+# print(c)
+
+
 #
 # #### Zad 5 ####
 # print("\nZadanie 5\n")
@@ -49,6 +52,10 @@ import  math
 # for x in g.flat:
 #     a.append(math.sin(x))
 # print(a)
+# ###lub###
+# a = np.sin(g)
+# print(a)
+
 #
 # #### Zad 6 ####
 # print("\nZadanie 6\n")
@@ -58,11 +65,17 @@ import  math
 # for x in h.flat:
 #     b.append(math.cos(x))
 # print(b)
+###lub###
+# b = np.cos(h)
+# print(b)
 #
 # #### Zad 7 ####
 # print("\nZadanie 7\n")
 #
-# print(g+h)
+# print(a+b)
+###lub###
+# dodawanie = np.add(a,b)
+# print(dodawanie)
 #
 #### Zad 8 ####
 # print("\nZadanie 8\n")
@@ -87,11 +100,16 @@ import  math
 # print(k.reshape((27,3)))
 
 #### Zad 11 ####
-print("\nZadanie 11\n")
-
-l = np.random.randint(0,99,12).reshape((3,4))
-print(l.flat)
-m = l.reshape((4,3))
-print(m.flat)
-n = l.reshape((2,6))
-print(n.flat)
+# print("\nZadanie 11\n")
+#
+# a = np.random.randint(0,99,12)
+# print(a)
+# macierz_1 = a.reshape(3, 4)
+# print(macierz_1)
+# print(macierz_1.ravel()) # ravel - spłaszczenie macirzy
+# macierz_2 = macierz_1.reshape(4,3)
+# print(macierz_2)
+# print(macierz_2.ravel())
+# macierz_3 = macierz_1.reshape(2,6)
+# print(macierz_3)
+# print(macierz_3.ravel())
