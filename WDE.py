aa0d87a943bd67c97bb78e4f5f14f37c81d89419
TKieda <tomasz.kieda@student.uwm.edu.pl>
2022-06-08 15:47:46 +0200

    "notatki"

--- WDE.py
+++ WDE.py
@@ -9,11 +9,26 @@ import pandas as pd
 
 #######ZAD 1########
 
-x=[1,5,6,7,8,9,12,14,16,19,20,21,22,23,26,28,30]
-y=[0.167,0.1675,0.1675,0.1678,0.1677,0.1674,0.1673,0.1683,0.1688,0.1689,0.1693,0.1687,0.1692,0.1691,0.1692,0.1697,0.1695]
-fig, ax = plt.subplots(figsize=(7.2,5.4))
-ax.plot(x,y,'r--')
-ax.set(xticks=np.arange(0,35, 5),yticks=np.arange(0.165,0.170,0.001), title='Wykres w sierpniu 2019')
-plt.text(29, 0.1685 ,'CZK',verticalalignment='baseline',horizontalalignment='right')
-ax.grid()
+# x=[1,5,6,7,8,9,12,14,16,19,20,21,22,23,26,28,30]
+# y=[0.167,0.1675,0.1675,0.1678,0.1677,0.1674,0.1673,0.1683,0.1688,0.1689,0.1693,0.1687,0.1692,0.1691,0.1692,0.1697,0.1695]
+# fig, ax = plt.subplots(figsize=(7.2,5.4))
+# ax.plot(x,y,'r--')
+# ax.set(xticks=np.arange(0,35, 5),yticks=np.arange(0.165,0.170,0.001), title='Wykres w sierpniu 2019')
+# plt.text(29, 0.1685 ,'CZK',verticalalignment='baseline',horizontalalignment='right')
+# ax.grid()
+# plt.show()
+
+#######ZAD 2########
+
+
+
+
+xlsx = pd.ExcelFile('odpady24.xlsx')
+df = pd.read_excel(xlsx, header=0)
+print(df)
+grupa = df.groupby('Rodzaje odpadów')['Wartość'].sum()
+grupa.plot(kind='pie', autopct='%.2f %%', fontsize=15, figsize=(10,10), legend=(5,5), explode=(0.2,0.2,0.2,0.2,0.2), label='')
+plt.title('Info', fontsize=20)
+plt.legend(loc='upper left')
+
 plt.show()
